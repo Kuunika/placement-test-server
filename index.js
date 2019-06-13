9d29415ae88dc0a93ef9018740ddc5306b1dbef4
ibwighane <bsmwalwanda@gmail.com>
2019-06-13 09:33:26 +0200

    modified the endpoints

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ const data = require('./data');
 app.get('/test-case-1', (req, res) => res.json(data.input1))
 app.get('/test-case-2', (req, res) => res.json(data.input2))
 app.get('/test-case-3', (req, res) => res.json(data.input3))
+app.get('/test-case-4', (req, res) => res.json(data.input4))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
